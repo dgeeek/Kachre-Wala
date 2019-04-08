5f383ef83121641b1cb131484d9f3ecda1ea1a33
arpit7786 <arpit7786@gmail.com>
2019-04-08 22:51:52 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/java/com/example/kachrewala/DriverMapActivity.java

--- app/src/main/java/com/example/kachrewala/DriverMapActivity.java
+++ app/src/main/java/com/example/kachrewala/DriverMapActivity.java
@@ -128,7 +128,7 @@ public class DriverMapActivity extends FragmentActivity implements OnMapReadyCal
         }
         LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
         //mFusedLocationClient.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
-        //mFusedLocationClient.requestLocationUpdates(mGoogl
+        //mFusedLocationClient.requestLocationUpdates(mGoogleApi,
 
     }
 
